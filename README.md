b176f3c86b8faf152b598aba399d4813eb32bf30
SOFTOV\hadar.m <macdasi@gmail.com>
2017-04-27 11:30:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## to start
+https://medium.com/react-weekly/react-native-and-typescript-ad57b7413ead
+
+## and
+1 terminal - npm run build -- --watch
+2 terminal npm start (will load dependency graph)
---
+++ src/index.tsx
@@ -0,0 +1,44 @@
+/**
+ * Created by hadar.m on 27/04/2017.
+ */
+import React, { Component } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export default class App extends Component<Props, State> {
+    render() {
+            return (
+                <View style={styles.container}>
+            <Text style={styles.text}>
+            Welcome to React Native!
+            </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    } as React.ViewStyle,
+
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    } as React.TextStyle,
+});
